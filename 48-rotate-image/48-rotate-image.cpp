50d3bab2bf39cdce85a8df1244c136e93ecefa7f
deezeddd <63869880+deezeddd@users.noreply.github.com>
2022-09-24 14:26:27 +0530

    Time: 4 ms (42.79%), Space: 7.2 MB (33.43%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void f(vector<vector<int>>& mat ,int n ,int m){
+        for(int i = 0;i < n ;i++){
+            for(int j = 0 ;j < i ; j++){
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+        for(int i = 0; i < n ;i++ ){
+            reverse(mat[i].begin(),mat[i].end());
+        }
+        
+    }
+    
+    void rotate(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        f(mat,n,m);
+    }
+};
